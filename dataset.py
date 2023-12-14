535c5b2e13cf2624dc55dc9d5c4c224b1f91672e
underwaterIker <ikeralonsosuarez@gmail.com>
2023-12-14 02:48:47 +0100

    TODO: finish main.py

--- dataset.py
+++ dataset.py
@@ -1,18 +1,30 @@
+# Import libraries
+import os
+import sys
+from pathlib import Path
+
 import fiftyone as fo
 import fiftyone.zoo as foz
 from fiftyone import ViewField as F
 
 import shutil
 
+# Get yolov5 root directory
+FILE = Path(__file__).resolve()
+ROOT = FILE.parents[0]  # YOLOv5 root directory
+if str(ROOT) not in sys.path:
+    sys.path.append(str(ROOT))  # add ROOT to PATH
+ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
 
-def download_dataset():
+
+def download_dataset(classes_selected: list):
     # Load dataset in Fiftyone format
     name = "open-images-v7"
     splits = ["train", "validation"]
     label_types = ["detections"]
-    classes = ["Tin can", "Apple", "Pear"]
-    FiftyoneDataset_dir = "D:\projects\TFG\yolov5\datasets\FiftyoneDataset"
-    max_samples = 10
+    classes = classes_selected  # classes_selected=["Tin can", "Apple", "Pear"]
+    FiftyoneDataset_dir = str(ROOT / "datasets/FiftyoneDataset")  # "D:\projects\TFG\yolov5\datasets\FiftyoneDataset"
+    max_samples = len(classes_selected)*10  # change to *200 or more after testing
 
     dataset = foz.load_zoo_dataset(
         name,
@@ -26,7 +38,7 @@ def download_dataset():
 
     # Export dataset in Fiftyone format to YOLOv5 format
     view = dataset.filter_labels("ground_truth", F("label").is_in(classes))
-    export_dir = "D:\projects\TFG\yolov5\datasets\YOLOv5Dataset_1"
+    export_dir = str(ROOT / "datasets/YOLOv5Dataset_1")  # "D:\projects\TFG\yolov5\datasets\YOLOv5Dataset_1" || ROOT / 'datasets/YOLOv5Dataset_1'
     label_field = "ground_truth"
 
     for split in splits:
@@ -42,8 +54,9 @@ def download_dataset():
         )
 
     # Remove the dataset in Fiftyone format, we don't need it, we only need dataset in YOLOv5 format
-    # shutil.rmtree(FiftyoneDataset_dir)
+    shutil.rmtree(FiftyoneDataset_dir)
 
 
 if __name__ == '__main__':
-    download_dataset()
+    classes_selected = ["Tin can", "Apple", "Pear"]
+    download_dataset(classes_selected)
--- main.py
+++ main.py
@@ -1,17 +1,40 @@
+# Import libraries
 import os
 import sys
 from pathlib import Path
 
+# Import files
 import detect
+import train
+import dataset
 
+# Get yolov5 root directory
 FILE = Path(__file__).resolve()
 ROOT = FILE.parents[0]  # YOLOv5 root directory
 if str(ROOT) not in sys.path:
     sys.path.append(str(ROOT))  # add ROOT to PATH
 ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
 
-detect.run(
-    weights=ROOT / 'yolov5s.pt',
-    source=ROOT / '0',
-    classes=[0, 5], # 0: person | 5:bus (see coco128.yaml)
-)
+
+# Downloading dataset function
+def downloading_dataset(classes_selected: list):
+    dataset.download_dataset(classes_selected)
+
+
+# Training model with a dataset
+def training():
+    pass # TODO
+
+
+# Detection function
+def detection(weights_selected: str, classes_selected: list):
+    detect.run(
+        weights=ROOT / weights_selected, # weights_selected='yolov5s.pt'
+        source=ROOT / '0',
+        classes=classes_selected, # classes_selected=[0, 5] || 0: person | 5:bus (see coco128.yaml)
+    )
+
+
+if __name__ == '__main__':
+    classes_selected = ["Tin can", "Apple", "Pear"]
+    downloading_dataset(classes_selected)
