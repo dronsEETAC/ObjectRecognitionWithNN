ab02c7f371807c265d853f64e5f7e8e0224ddc49
underwaterIker <ikeralonsosuarez@gmail.com>
2024-01-18 13:39:03 +0100

    main_app.py: ground station done. Next to do: on board

--- DEE_addition/main_app.py
+++ DEE_addition/main_app.py
@@ -1,4 +1,5 @@
 # IMPORT LIBRARIES
+import time
 import tkinter as tk
 from tkinter import messagebox, ttk
 from PIL import ImageTk, Image
@@ -15,6 +16,8 @@ import hubconf
 object = None
 detectionPlace = None
 showStream = False
+autopilotConnected = False
+takenOff = False
 
 
 # ROOT CONFIGURATION
@@ -29,11 +32,19 @@ root.iconbitmap('assets/drone.ico')
 # FUNCTIONS
 def EndConnections():
     try:
-        videoStream_client.publish("VideoStreamClient/cameraService/stopVideoStream")
         videoStream_client.unsubscribe("cameraService/VideoStreamClient/videoFrame")
+        videoStream_client.publish("VideoStreamClient/cameraService/stopVideoStream")
         videoStream_client.loop_stop()
     except:
         pass
+
+    try:
+        autopilot_client.publish("AutopilotClient/autopilotService/disconnect")
+        autopilot_client.loop_stop()
+
+    except:
+        pass
+
     root.destroy()
 
 
@@ -131,12 +142,23 @@ def on_connect_autopilot(external_client, userdata, flags, rc):
 def connect():
     global videoStream_client
     global autopilot_client
+    global autopilotConnected
+
+    # Get the quality and period values
+    quality = quality_slider.get()
+    period = period_slider.get()
 
     # Just in case the Connect button is pressed more than once
     try:
-        videoStream_client.publish("VideoStreamClient/cameraService/stopVideoStream")
         videoStream_client.unsubscribe("cameraService/VideoStreamClient/videoFrame")
+        videoStream_client.publish("VideoStreamClient/cameraService/stopVideoStream")
         videoStream_client.loop_stop()
+    except:
+        pass
+
+    try:
+        autopilot_client.publish("AutopilotClient/autopilotService/disconnect")
+        autopilot_client.loop_stop()
     except:
         pass
 
@@ -170,47 +192,100 @@ def connect():
             videoStream_client.model = model
 
             # Publish startVideoStream and subscribe to videoFrame (cameraService)
-            videoStream_client.publish("VideoStreamClient/cameraService/startVideoStream")
+            videoStream_client.publish(f"VideoStreamClient/cameraService/startVideoStream", f"{quality}/{period}")
             videoStream_client.subscribe("cameraService/VideoStreamClient/videoFrame")
             videoStream_client.loop_start()
+
+            # Publish connect order (autopilotService)
+            autopilot_client.publish("AutopilotClient/autopilotService/connect")
+            autopilotConnected = True
 
     except:
         messagebox.showerror("Error", "Please try again.")
 
 
+def arm_takeoff():
+    global takenOff
+    if autopilotConnected is True:
+        # Publish armDrone & takeOff orders (autopilotService)
+        autopilot_client.publish("AutopilotClient/autopilotService/armDrone")
+        autopilot_client.publish("AutopilotClient/autopilotService/takeOff", "2")
+        takenOff = True
+    else:
+        messagebox.showerror("Error", "Please press CONNECT first.")
+
+
+def flyNorth():
+    if autopilotConnected is True:
+        if takenOff is True:
+            # Publish go North order (autopilotService)
+            autopilot_client.publish("AutopilotClient/autopilotService/go", "North")
+        else:
+            messagebox.showerror("Error", "Please ARM & TAKE-OFF first.")
+    else:
+        messagebox.showerror("Error", "Please press CONNECT first.")
+
 
 # LABELS, COMBOBOXES, BUTTONS
+# detection parameters label
+detectionParameters_label = tk.Label(root, text='Detection parameters:', bg="dark orange")
+detectionParameters_label.grid(row=0, column=0, sticky="WE", padx=10, pady=(20, 10))
+
 # objects dropdown menu
 objects = ['Tin can', 'Book']
 objects_label = tk.Label(root, text='Select an object:')
-objects_label.grid(row=1, column=0, sticky="W", padx=10, pady=10)
+objects_label.grid(row=1, column=0, sticky="W", padx=10, pady=(0, 10))
 objects_dropdown = ttk.Combobox(root, state='readonly', values=objects)
 objects_dropdown.bind("<<ComboboxSelected>>", lambda e: root.focus()) # to remove highlighting of the chosen object
-objects_dropdown.grid(row=2, column=0, sticky="W", padx=10)
+objects_dropdown.grid(row=2, column=0, sticky="W", padx=10, pady=(0, 10))
 
-# place of detection dropdown menu
+# Place of detection dropdown menu
 detectionPlaces = ['On board', 'Ground station']
 detectionPlaces_label = tk.Label(root, text='Select where you want the detection to take place:')
-detectionPlaces_label.grid(row=3, column=0, sticky="W", padx=10, pady=10)
+detectionPlaces_label.grid(row=3, column=0, sticky="W", padx=10, pady=(0, 10))
 detectionPlaces_dropdown = ttk.Combobox(root, state='readonly', values=detectionPlaces)
 detectionPlaces_dropdown.bind("<<ComboboxSelected>>", lambda e: root.focus()) # to remove highlighting of the chosen object
-detectionPlaces_dropdown.grid(row=4, column=0, sticky="W", padx=10)
+detectionPlaces_dropdown.grid(row=4, column=0, sticky="W", padx=10, pady=(0, 10))
 
-# connect button
-connect_button = tk.Button(root, text='CONNECT', command=connect)
-connect_button.grid(row=5, column=0, sticky="WE", pady=10)
+# Image parameters label
+imageParameters_label = tk.Label(root, text='Video stream Image parameters:', bg="dark orange")
+imageParameters_label.grid(row=5, column=0, sticky="WE", padx=10, pady=(10, 10))
 
-# video stream buttons
-showStream_button = tk.Button(root, text='Show stream', command=show_stream)
-showStream_button.grid(row=0, column=1, pady=10)
-stopStream_button = tk.Button(root, text='Stop stream', command=stop_stream)
-stopStream_button.grid(row=0, column=2, pady=10)
+# Quality and Period sliders
+quality_slider = tk.Scale(root, label="Quality:", resolution=10, from_=10, to=100, tickinterval=10, orient=tk.HORIZONTAL)
+quality_slider.set(50)
+quality_slider.grid(row=6, column=0, sticky="WE", pady=(0, 10))
+period_slider = tk.Scale(root, label="Period:", resolution=0.1, from_=0.1, to=1, tickinterval=0.1, orient=tk.HORIZONTAL)
+period_slider.set(0.2)
+period_slider.grid(row=7, column=0, sticky="WE", pady=(0, 10))
+
+# Connect button
+connect_button = tk.Button(root, text='CONNECT', font='sans 13 bold', command=connect)
+connect_button.grid(row=8, column=0, sticky="WE", pady=(10, 10))
+
+# Drone operations label
+droneOperations_label = tk.Label(root, text='Drone operations:', bg="dark orange")
+droneOperations_label.grid(row=9, column=0, sticky="WE", padx=10, pady=(10, 10))
+
+# Arm & Take-off button
+arm_takeoff_button = tk.Button(root, text='ARM & TAKE-OFF', font='sans 13 bold', command=arm_takeoff)
+arm_takeoff_button.grid(row=10, column=0, sticky="WE", pady=(0, 10))
+
+# Fly North button
+flyNorth_button = tk.Button(root, text='FLY NORTH', font='sans 13 bold', command=flyNorth)
+flyNorth_button.grid(row=11, column=0, sticky="WE", pady=(0, 10))
+
+# Video stream buttons
+showStream_button = tk.Button(root, text='Show stream', font='sans 12 bold', command=show_stream)
+showStream_button.grid(row=0, column=1, pady=(20, 5))
+stopStream_button = tk.Button(root, text='Stop stream', font='sans 12 bold', command=stop_stream)
+stopStream_button.grid(row=0, column=2, pady=(20, 5))
 
 # Label to show video stream
 img = Image.open('assets/videoStream_label-size.jpg')
 tkimage = ImageTk.PhotoImage(img)
 videoStream_label = tk.Label(root, image=tkimage)
-videoStream_label.grid(row=1, column=1, rowspan=1000, columnspan=2)
+videoStream_label.grid(row=1, column=1, rowspan=1000, columnspan=2, padx=30)
 
 
 # RUN THE APP
